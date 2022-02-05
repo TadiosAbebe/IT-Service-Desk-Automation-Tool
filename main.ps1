4b940caaea6d82c7a72610d3139de24019ee3cd7
tadios <besttadios@gmail.com>
2022-02-05 21:38:26 +0300

    Added the logic to enable firewall
    📝Created a function to check firewall status
    📝Created a function to output a message on the output box

--- main.ps1
+++ main.ps1
@@ -1,3 +1,31 @@
+$btn_firewall_enable_Click = { enable_firewall }
+
+function enable_firewall {
+    Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled True
+    check_firewall_stat
+}
+function check_firewall_stat() {
+    $firewall_profile = (get-NetFirewallProfile)
+    $firewall_profile | ForEach-Object {
+        if ($_.Enabled -eq 1) {
+            write-host "the windows firewall $($_.Name) profile is enabled" -foregroundcolor Green
+            Add-OutputBoxLine -Message "Windows firewall is enabled for $($_.Name)"
+        }
+        else {
+            write-host "the windows filewall $($_.Name) profile is disabled" -foregroundcolor Red
+            Add-OutputBoxLine -Message "Windows firewall is disabled for $($_.Name)"
+        }
+    };
+    Write-Host "`n"
+}
+
+function Add-OutputBoxLine {
+    Param ($Message)
+    $OutputBox.AppendText("`r`n$Message")
+    $OutputBox.Refresh()
+    $OutputBox.ScrollToCaret()
+}
+
 Add-Type -AssemblyName System.Windows.Forms
 . (Join-Path $PSScriptRoot 'ui.ps1')
 [System.Windows.Forms.Application]::EnableVisualStyles()
