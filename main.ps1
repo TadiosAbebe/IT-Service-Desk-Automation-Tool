9e12b0e78699ea44803dbadf5f3580b43150f23d
tadios <besttadios@gmail.com>
2022-02-05 21:41:51 +0300

    Added the logic the check the scripts privilage
    📝If the script is running under normal mode, it elevates the privilage

--- main.ps1
+++ main.ps1
@@ -1,3 +1,10 @@
+$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+$testadmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+if ($testadmin -eq $false) {
+    Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    exit $LASTEXITCODE
+}
+
 $btn_firewall_enable_Click = { enable_firewall }
 
 function enable_firewall {
