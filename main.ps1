fb2a829bfe13bff81cb9b58e8efd0a4c2ccd3595
tadios <besttadios@gmail.com>
2022-02-06 18:08:49 +0300

    Added a function nondebug_mode to hide the console

--- main.ps1
+++ main.ps1
@@ -384,6 +384,14 @@ function Add-OutputBoxLine {
     $OutputBox.Refresh()
     $OutputBox.ScrollToCaret()
 }
+function nondebug_mode() {
+    Add-Type -Name Window -Namespace Console -MemberDefinition '
+	[DllImport("Kernel32.dll")]
+	public static extern IntPtr GetConsoleWindow();
+	[DllImport("user32.dll")]
+	public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);'
+    [Console.Window]::ShowWindow([Console.Window]::GetConsoleWindow(), 0)
+}
 
 Add-Type -AssemblyName System.Windows.Forms
 . (Join-Path $PSScriptRoot 'ui.ps1')
